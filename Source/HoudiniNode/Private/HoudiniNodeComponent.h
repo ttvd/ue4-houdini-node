341210e19a31d027a377966920ca2b934e3b674c
Mykola Konyk <mykola@konyk.org>
2018-01-25 18:07:01 -0800

    More offset hiding.

--- Source/HoudiniNode/Private/HoudiniNodeComponent.h
+++ Source/HoudiniNode/Private/HoudiniNodeComponent.h
@@ -14,6 +14,34 @@ public:
 
 public:
 
+    //! Return current scratch buffer pointer.
+    char* GetCurrentScratchSpacePosition() const;
+
+    //! Return current offset.
+    uint32 GetCurrentScratchSpaceOffset() const;
+
+public:
+
+    //! Increment scratch buffer offset.
+    void IncrementScratchSpaceBufferOffset(uint32 Offset);
+
+    //! Increment scratch buffer offset for a given type.
+    template<typename TType> void IncrementScratchSpaceBufferOffset();
+
+protected:
+
     //! Scratch space buffer.
-    char ScratchSpaceBuffer[65536];
+    char ScratchSpaceBuffer[65536u];
+
+    //! Current scratch buffer offset.
+    uint32 ScratchSpaceBufferOffset;
 };
+
+
+template<typename TType>
+void
+UHoudiniNodeComponent::IncrementScratchSpaceBufferOffset()
+{
+    const uint32 TypeSize = sizeof(TType);
+    IncrementScratchSpaceBufferOffset(TypeSize);
+}

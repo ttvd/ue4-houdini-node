f728c2987c489e6edd19d67d6e233bc946869a6f
Mykola Konyk <mykola@konyk.org>
2018-01-27 17:33:42 -0800

    Adding loading and unloading of the library.

--- Source/HoudiniNode/Private/HoudiniNodeAsset.cpp
+++ Source/HoudiniNode/Private/HoudiniNodeAsset.cpp
@@ -7,3 +7,23 @@ UHoudiniNodeAsset::UHoudiniNodeAsset(const FObjectInitializer& ObjectInitializer
 {
 
 }
+
+
+bool
+UHoudiniNodeAsset::GetFilename(UT_String& RawFilename) const
+{
+    UT_String = "";
+
+#if WITH_EDITORONLY_DATA
+
+    const FString& Filename = AssetImportData->GetFirstFilename;
+    if(!AssetFilename.IsEmpty())
+    {
+        RawFilename = TCHAR_TO_UTF8(*AssetFilename);
+        return true;
+    }
+
+#endif
+
+    return false;
+}
--- Source/HoudiniNode/Private/HoudiniNodeClass.h
+++ Source/HoudiniNode/Private/HoudiniNodeClass.h
@@ -3,6 +3,8 @@
 
 
 class OBJ_Node;
+class OP_OTLLibrary;
+
 class UHoudiniNodeAsset;
 
 
@@ -18,12 +20,31 @@ public:
     UPROPERTY()
     UHoudiniNodeAsset* HoudiniNodeAsset;
 
+public:
+
+    //! Create library for the underlying asset.
+    bool AddLibrary();
+
+    //! Remove library for the underlying asset.
+    bool RemoveLibrary();
+
+    //! Return true if the library for the underlying asset has been created.
+    bool HasLibrary() const;
+
+protected:
+
+    //! Retrieve asset names for this Houdini node asset.
+    bool GetAssetNames(TArray<FString>& AssetNames) const;
+
 protected:
 
     //! List of generated properties.
     TMap<FString, UProperty*> Properties;
 
 protected:
+
+    //! Corresponding loaded library.
+    OP_OTLLibrary* Library;
 
     //! Corresponding Houdini object node.
     OBJ_Node* Node;
